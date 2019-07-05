05b36869e63f392b92f66d842a4277ef3904bc46
PapiAlfa <49841009+PapiAlfa@users.noreply.github.com>
2019-07-05 14:56:57 +0100

    Add files via upload

---
+++ AlfaRemoveAICops/client.lua
@@ -0,0 +1,11 @@
+-- Alfa's Present to The MMORP crew!
+-- Enjoy, can't wait until the server is up!
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local playerPed = GetPlayerPed(-1)
+        local playerLocalisation = GetEntityCoords(playerPed)
+        ClearAreaOfCops(playerLocalisation.x, playerLocalisation.y, playerLocalisation.z, 400.0)
+    end
+end)
